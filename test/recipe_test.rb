2540ea9857ad582d85c8f28a2acae064a9007dcb
David Holtkamp <david.hholtkamp@gmail.com>
2019-11-05 11:04:50 -0700

    Wrote tests for recipe class other than total calories

---
+++ test/recipe_test.rb
@@ -0,0 +1,81 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+require './lib/recipe'
+
+class RecipeTest < Minitest::Test
+
+  def setup
+    @cheese = Ingredient.new("Cheese", "C", 100)
+    @mac = Ingredient.new("Macaroni", "oz", 30)
+    @mac_and_cheese = Recipe.new("Mac and Cheese")
+  end
+
+  def test_it_exists
+    assert_instance_of Recipe, @mac_and_cheese
+  end
+
+  def test_it_initializes
+    assert_equal "Mac and Cheese", @mac_and_cheese.name
+  end
+
+  def test_ingredients_required
+    expected = Hash.new(0)
+
+    assert_equal expected, @mac_and_cheese.ingredients_required
+  end
+
+  def test_recipe_can_add_ingredients
+    @mac_and_cheese.add_ingredient(@cheese, 2)
+    @mac_and_cheese.add_ingredient(@mac, 8)
+
+    expected = {
+          @cheese => 2,
+          @mac => 8
+    }
+
+    assert_equal expected, @mac_and_cheese.ingredients_required
+  end
+
+  def test_amount_required
+
+    @cheese = Ingredient.new("Cheese", "C", 100)
+    @mac = Ingredient.new("Macaroni", "oz", 30)
+    @mac_and_cheese = Recipe.new("Mac and Cheese")
+
+    @mac_and_cheese.add_ingredient(@cheese, 2)
+    @mac_and_cheese.add_ingredient(@mac, 8)
+
+    assert_equal 2, @mac_and_cheese.amount_required(@cheese)
+    assert_equal 8, @mac_and_cheese.amount_required(@mac)
+  end
+
+  def test_ingredients
+
+    @cheese = Ingredient.new("Cheese", "C", 100)
+    @mac = Ingredient.new("Macaroni", "oz", 30)
+    @mac_and_cheese = Recipe.new("Mac and Cheese")
+
+    @mac_and_cheese.add_ingredient(@cheese, 2)
+    @mac_and_cheese.add_ingredient(@mac, 8)
+
+    assert_equal [@cheese, @mac], @mac_and_cheese.ingredients
+
+  end
+
+  def test_total_calories
+
+    @cheese = Ingredient.new("Cheese", "C", 100)
+    @mac = Ingredient.new("Macaroni", "oz", 30)
+    @mac_and_cheese = Recipe.new("Mac and Cheese")
+
+    @mac_and_cheese.add_ingredient(@cheese, 2)
+    @mac_and_cheese.add_ingredient(@mac, 8)
+
+    assert_equal 440, @mac_and_cheese.total_calories
+
+  end
+
+
+
+end
